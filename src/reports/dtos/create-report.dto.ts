7b8e02157b4c9bae12137995506fca493b87ec72
Moopz <pongzthor@gmail.com>
2024-12-11 15:24:33 +0700

    feat: implements create report endpoint

---
+++ src/reports/dtos/create-report.dto.ts
@@ -0,0 +1,39 @@
+import {
+  IsLatitude,
+  IsLongitude,
+  IsNotEmpty,
+  IsNumber,
+  IsString,
+  Max,
+  Min,
+} from 'class-validator';
+
+export class CreateReportDto {
+  @IsNumber()
+  @Min(0)
+  price: number;
+
+  @IsString()
+  @IsNotEmpty()
+  make: string;
+
+  @IsString()
+  @IsNotEmpty()
+  model: string;
+
+  @IsNumber()
+  @Min(1930)
+  @Max(2030)
+  year: number;
+
+  @IsLongitude()
+  lng: number;
+
+  @IsLatitude()
+  lat: number;
+
+  @IsNumber()
+  @Min(0)
+  @Max(1_000_000)
+  milage: number;
+}
